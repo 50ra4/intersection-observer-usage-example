cade6d661ca9a23a49ffc4a003f2ca96b83e96e4
50ra4 <50ra4.dev.dev@gmail.com>
2022-12-18 14:57:47 +0900

    refactor: TopPage

--- src/presentation/pages/TopPage/TopPage.tsx
+++ src/presentation/pages/TopPage/TopPage.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { PageTemplate } from 'src/presentation/PageTemplate';
 
 function TopPage() {
   return (
-    <main>
-      <h2>TopPage</h2>
-      <div>TopPage component</div>
-      <ul>
+    <PageTemplate>
+      <List>
         <li>
-          <Link to="/1">Page1</Link>
+          <Link to="/examples/1">
+            1. 1番下までスクロールするとボタンが活性化する
+          </Link>
         </li>
         <li>
-          <Link to="/2">Page2</Link>
+          <Link to="/examples/2">
+            2. スクロールしていくとサブタイトル通過後、URLに#が追加される
+          </Link>
         </li>
         <li>
-          <Link to="/3">Page3</Link>
+          <Link to="/examples/3">3. 無限スクロール</Link>
         </li>
-      </ul>
-    </main>
+      </List>
+    </PageTemplate>
   );
 }
+
+const List = styled.ul`
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > li + li {
+    margin-top: 8px;
+  }
+`;
 
 export default TopPage;
